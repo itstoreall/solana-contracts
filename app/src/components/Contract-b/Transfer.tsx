02f5a656da4591dfef14bffb38f4d71491edbf30
Serhii Stanislav <serhiistanislav@gmail.com>
2022-05-07 20:47:40 +0300

    adds contract-state

--- app/src/components/Contract-b/Transfer.tsx
+++ app/src/components/Contract-b/Transfer.tsx
@@ -1,4 +1,142 @@
 // @ts-nocheck
+import { useState, useEffect, useContext, useMemo } from 'react';
+import AContext from './context';
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from '@solana/web3.js';
+import { transfer as log } from '../../logs/contract-a.js';
+
+const TransferComponent = () => {
+  const [trustedPublicKey, setTrustedPublicKey] = useState(null);
+  const [theHash, setTheHash] = useState(null);
+
+  const { accountID } = useContext(AContext);
+  const lamports = 10000001;
+
+  useMemo(() => log.process(), []);
+
+  // ---
+  const getProvider = () => {
+    const provider = 'solana' in window && window.solana;
+
+    if (provider.isPhantom) {
+      console.log('Phantom provider:', provider.isPhantom);
+      return provider;
+    }
+  };
+
+  const provider = getProvider();
+
+  useEffect(() => {
+    if (!provider) return;
+
+    const trustedConnect = async () => {
+      try {
+        const { publicKey: trustedPublicKey } = await window.solana.connect({
+          onlyIfTrusted: true,
+        });
+        trustedPublicKey && setTrustedPublicKey(trustedPublicKey);
+      } catch (error) {
+        connectPhantom();
+      }
+    };
+
+    trustedConnect();
+
+    return () => provider.disconnect();
+    // eslint-disable-next-line
+  }, []);
+
+  const connectPhantom = async () => {
+    try {
+      const resp = await window.solana.connect();
+      resp.publicKey.toString();
+    } catch (err) {
+      console.log('ERROR in Phantom connect:', err.message);
+    }
+  };
+
+  window.solana.on('connect', () => console.log('Phantom connected!'));
+  window.solana.on('disconnect', () => console.log('Phantom disconnected!'));
+
+  console.log('isConnected:', window.solana.isConnected);
+  console.log('Trusted publicKey:', trustedPublicKey);
+
+  const makeTransfer = async () => {
+    try {
+      if (!trustedPublicKey) return;
+
+      const url = clusterApiUrl('devnet');
+      const connection = url && new Connection(url);
+      const recipientPublicKey = new PublicKey(accountID);
+
+      const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: trustedPublicKey,
+          toPubkey: recipientPublicKey,
+          lamports: lamports,
+        })
+      );
+
+      transaction.feePayer = trustedPublicKey;
+
+      transaction.recentBlockhash = (
+        await connection.getLatestBlockhash()
+      ).blockhash;
+
+      const { signature } = await window.solana.signAndSendTransaction(
+        transaction
+      );
+
+      await connection.confirmTransaction(signature);
+
+      console.log('connection -->', connection);
+      console.log('transaction -->', transaction);
+      console.log('signature -->', signature);
+      console.log('transaction.feePayer -->', transaction.feePayer);
+    } catch (err) {
+      console.log('ERROR in makeTransfer:', err.message);
+    }
+  };
+
+  makeTransfer();
+
+  // ---
+
+  return (
+    <div>
+      <>
+        <div>
+          <span style={{ display: 'inline-flex', width: '150px' }}>
+            transfer hash:
+          </span>
+          {theHash && (
+            <span
+              style={{
+                display: 'inline-flex',
+                width: '150px',
+                fontSize: '12px',
+              }}
+            >
+              {`${theHash?.slice(0, 10)}...`}
+            </span>
+          )}
+          <button onClick={() => setTheHash(null)}>again</button>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default TransferComponent;
+
+// =============================================
+
+/*
 import { useState, useContext, useMemo } from 'react';
 import AContext from './context';
 import {
@@ -85,3 +223,4 @@ const TransferComponent = () => {
 };
 
 export default TransferComponent;
+*/
---
+++ notes/counter-state.js
@@ -0,0 +1,26 @@
+/*
+
+keypair:
+mkdir solana-wallet
+solana-keygen new --outfile solana-wallet/keypair.json
+
+pubkey: 2nMUYqTgXVPxuBiFEFYmTbMCnACCVfWybyk8gy4eeUzB
+===========================================================================
+ordinary target year rare magic rain nation bridge early magic bunker equip
+---
+
+solana airdrop 1 $(solana-keygen pubkey solana-wallet/keypair.json)
+
+solana account $(solana-keygen pubkey solana-wallet/keypair.json)
+
+Public Key: 2nMUYqTgXVPxuBiFEFYmTbMCnACCVfWybyk8gy4eeUzB
+Balance: 3 SOL
+Owner: 11111111111111111111111111111111
+Executable: false
+Rent Epoch: 307
+---
+
+https://youtu.be/uUfhqHBoQpU
+https://github.com/max-block/solana-examples/tree/v0.1.1/01__state
+
+*/
